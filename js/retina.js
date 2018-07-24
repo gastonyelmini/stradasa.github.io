7a478a2914d54d0fd23a30932b7896d12de66e26
Gaston Yelmini <gastonyelmini@gmail.com>
2018-07-23 22:53:30 -0300

    nuevo logo y texto

--- js/retina.js
+++ js/retina.js
@@ -4,14 +4,14 @@ jQuery(window).load(function () {
         var retinaEl = jQuery("#logo img.logo-main");
         var retinaLogoW = retinaEl.width();
         var retinaLogoH = retinaEl.height();
-        retinaEl.attr("src", "content/eco/images/retina-eco.png").width(retinaLogoW).height(retinaLogoH);
+        retinaEl.attr("src", "content/eco/images/logo-white.png").width(retinaLogoW).height(retinaLogoH);
         var stickyEl = jQuery("#logo img.logo-sticky");
         var stickyLogoW = stickyEl.width();
         var stickyLogoH = stickyEl.height();
-        stickyEl.attr("src", "content/eco/images/retina-eco.png").width(stickyLogoW).height(stickyLogoH);
+        stickyEl.attr("src", "content/eco/images/logo-white.png").width(stickyLogoW).height(stickyLogoH);
         var mobileEl = jQuery("#logo img.logo-mobile");
         var mobileLogoW = mobileEl.width();
         var mobileLogoH = mobileEl.height();
-        mobileEl.attr("src", "content/eco/images/retina-eco.png").width(mobileLogoW).height(mobileLogoH);
+        mobileEl.attr("src", "content/eco/images/logo-white.png").width(mobileLogoW).height(mobileLogoH);
     }
 });
