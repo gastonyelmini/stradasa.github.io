326034f342ea7567f8a08f80057b20c6fa311775
Gaston Yelmini <gastonyelmini@gmail.com>
2018-09-18 09:57:34 -0300

    Add renders and position

--- js/renders-aria.js
+++ js/renders-aria.js
@@ -4,18 +4,25 @@ $(document).ready(function ($) {
     $("#aria3").hide();
     $("#aria4").hide();
     $("#aria5").hide();
+    $("#aria6").hide();
+    $("#aria7").hide();
+    $("#aria8").hide();
+    $("#aria9").hide();
+    $("#aria10").hide();
+    $("#aria11").hide();
+    $("#aria12").hide();
 
     function durationSlider() {
-        var listItems = 5;
+        var listItems = 12;
         var count = 1;
 
         setInterval(function () {;
 
-            if (count != 5) {
+            if (count != 12) {
                 $("#aria" + count).hide();
                 $("#aria" + (count + 1)).fadeIn(300);
-            } else if (count == 5) {
-                $("#aria5").hide()
+            } else if (count == 12) {
+                $("#aria12").hide()
                 $("#aria1").fadeIn(300);
             }
             console.log("Oculte" + count);
