071b54c08d8d022edbbd3036a462930a84d3fffc
Gaston Yelmini <gastonyelmini@gmail.com>
2018-10-01 16:14:11 -0300

    New logo positions

--- js/smooth.js
+++ js/smooth.js
@@ -1,33 +1,12 @@
-$(document).ready(function ($) {
-    // Selecciono todos los links con #
-    $('a[href*="#"]')
-        // Elimino los que no hacen nada
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function (event) {
-            if (
-                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
-                location.hostname == this.hostname
-            ) {
-                // Buscar elemento a escrolear
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Existe el target?
-                if (target.length) {
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function () {
-                        var $target = $(target);
-                        $target.focus();
-                        if ($target.is(":focus")) {
-                            return false;
-                        } else {
-                            $target.attr('tabindex', '-1');
-                            $target.focus();
-                        };
-                    });
-                }
-            }
-        });
+$(function($) {
+  // Select all links with hashes
+  $(".nav").click(function() {
+    $("li").removeClass("active");
+    var link = $(this).attr("href");
+    $(this)
+      .parent()
+      .addClass("active");
+    var posi = $(link).offset().top;
+    $("body,html").animate({ scrollTop: posi }, 700);
+  });
 });
